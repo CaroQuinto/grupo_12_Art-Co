6ad25f1988f29b5a4a366e3b55c654755c983a21
CaroQuinto <caroquintoman@gmail.com>
2021-06-01 00:26:41 -0300

    header y register

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-
-const path = require('path')
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/view/index.html'))
+app.get('/', (req,res) =>{
+    //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
+    res.sendFile(path.join(__dirname + '/views/index.html'));  // Permite enviar un archivo HTML
 });
-app.use(express.static(path.resolve(__dirname, './public')));
+app.get('/', (req,res) =>{
+    //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
+    res.sendFile(path.join(__dirname + '/css/styles.css'));  // Permite enviar un archivo HTML
+});
+app.get('/register.html', (req,res) =>{
+    //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
+    res.sendFile(path.join(__dirname + '/views/register.html'));  // Permite enviar un archivo HTML
+});
 
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3005,()=>console.log('Lo Lograste'));
+app.listen(3005,()=>console.log('Artico'));
 
